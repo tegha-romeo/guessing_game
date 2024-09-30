36ae1c1e64d03dda72a4915cd88f4b2eba569ea3
tegha-romeo <romeotegha@gmail.com>
2024-09-30 15:50:03 +0100

    i copy this code

--- guess_number.sh
+++ guess_number.sh
@@ -1,6 +1,6 @@
 # This is a guessing game
-guess_random() {
-    #we declare a random number and assign it to a  varriable called random_number
+    guess_random() {
+#we declare a random number and assign it to a  varriable called random_number
 rand_number=$((RANDOM % $1))
 for i in {0..2}; do
 echo "guess number"
